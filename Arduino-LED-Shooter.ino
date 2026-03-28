038ca2aba1c8ee49dc8c28aa545b58e031361e06
22ya-wq <mirsor1012@gmail.com>
2026-03-28 13:24:36 +0500

    Arduino-LED-Shooter.ino

--- Arduino-LED-Shooter.ino
+++ Arduino-LED-Shooter.ino
@@ -11,6 +11,7 @@
   int playerPosition = 0;
   int secretGoal = 0;
   int sp = 0;
+  uint32_t timer;
 
   void setup() {
     for (int i = 0; i < 5; i++) {
@@ -28,6 +29,9 @@
     secretGoal = random(0, 5);
 
     digitalWrite(leds2[secretGoal], HIGH);
+    timer = millis();
+
+    Serial.begin(9600);
   }
 
   //Reset old, unused leds on player led-line
@@ -44,6 +48,9 @@
     }
     secretGoal = random(0, 5);
     digitalWrite(leds2[secretGoal], HIGH);
+    tone(SPEAKER, 200);
+    delay(50);
+    noTone(SPEAKER);
   }
 
   //Get player position(0-4)
@@ -55,9 +62,32 @@
     }
   }
 
+  void sadMusic() {
+    tone(SPEAKER, 440, 200); 
+    delay(250);
+    tone(SPEAKER, 415, 200); 
+    delay(250);
+    tone(SPEAKER, 392, 200); 
+    delay(250);
+    tone(SPEAKER, 370, 400); 
+    delay(450);
+
+  }
+
   void loop() {
     int y = map(analogRead(J_VRY), 0, 1023, -1, 1);
     float speed = y;
+
+    if (millis() - timer >= 2000) {
+      timer = millis();
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(leds2[i], HIGH);
+      }
+
+      sadMusic();
+      timer = millis();
+      resetGoalLeds();
+    }
 
     //Checking for win condition
     if (digitalRead(12) == LOW && playerPosition == secretGoal) {
@@ -71,19 +101,20 @@
       noTone(SPEAKER);
 
       delay(1500);
+      timer = millis();
       resetGoalLeds();
     }
 
     //If player makes mistake
     if (digitalRead(12) == LOW && playerPosition != secretGoal) {
-      tone(SPEAKER, 440, 200); 
-      delay(250);
-      tone(SPEAKER, 415, 200); 
-      delay(250);
-      tone(SPEAKER, 392, 200); 
-      delay(250);
-      tone(SPEAKER, 370, 400); 
-      delay(450);
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(leds[i], HIGH);
+      }
+
+      sadMusic();
+      
+      resetPlayerLeds();
+      timer = millis();
       resetGoalLeds();
     }
     
